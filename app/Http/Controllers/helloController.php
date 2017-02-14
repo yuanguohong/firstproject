e1a2c616cdbb7d41ce65745e406d2c89eecf431b
lingao <yuanguohong@live.com>
2017-02-14 15:34:53 +0800

    first

---
+++ app/Http/Controllers/helloController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: huawen
+ * Date: 2017/2/14
+ * Time: 15:09
+ */
+
+namespace App\Http\Controllers;
+
+
+class helloController extends Controller{
+	public function index(){
+		echo "覃枭是菜ji";
+	}
+}
