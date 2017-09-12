c40b5f14db4e6cfbfc38360ae8bc118157cf7e9a
guohong <yuanguohong@live.com>
2017-09-13 02:21:05 +0800

    OK

--- app/Http/Controllers/webhooksController.php
+++ app/Http/Controllers/webhooksController.php
@@ -12,11 +12,7 @@ namespace App\Http\Controllers;
 class webhooksController extends Controller
 {
     public function index(){
-        error_reporting ( E_ALL );
         $dir = '/www/firstproject';//该目录为git检出目录
-        $handle = popen('cd '.$dir.' && git pull 2>&1','r');
-        $read = stream_get_contents($handle);
-        printf($read);
-        pclose($handle);
+        $handle = shell_exec('cd '.$dir.' && git pull 2>&1');
     }
 }
